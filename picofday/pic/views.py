d022535c2a8927faa3668391cd979ce163bff2a6
metealp <kizilcaymetealp@gmail.com>
2020-02-08 21:59:46 +0300

    Changed the HTML for the site.

--- picofday/pic/views.py
+++ picofday/pic/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'blog/index.html', {})
+    return render(request, 'pic/index.html', {})
